6997c4559a0fc7e592dc4ecf28710c22da4e77dd
leepsym <frazeth@whs.school.nz>
2024-05-16 09:29:09 +1200

    Update 0.0.0.3
    Replaced 'try catch' method from lines 22-25 with a 'throws' method in the function declaration.
    Added comments from line 27-107
    
    Overview:
    Cleaned up code
    Added comments

--- main/Game.java
+++ main/Game.java
@@ -11,22 +11,20 @@ public class Game {
     Fight fight = new Fight(); // Creates an instance of the Fight class
     public static Room currentRoom = new Room01(); // Creates a static variable that stores
 
-    public void playerAction() { // Used for letting the player take an action
+    public void playerAction() throws NullPointerException { // Used for letting the player take an action // If the console receives the error NullPointerException, the game ignores it and carries on
         System.out.println("What would you like to do?");
         String playerChoice = scanner.nextLine().toLowerCase(); // Gets a lower case version of an input from the player
         System.out.println();
         String[] words = playerChoice.split("\\s+", 2); // Separates the input from the user into an array with an index for each word
         String firstWord = words[0]; // Sets the first word in the array to the first word inputted
-        String restOfWords = ""; // Creates the String restOfWords, already initialised
+        String restOfWords; // Creates the String restOfWords, already initialised
         String commandType; // Creates the string commandType, to be used later, in the case of the user inputting 'help'
-        try { // Attempts to set restOfWords' to the second item in the array 'words'
-            restOfWords = words[1];
-        } catch (NullPointerException _){} // If the slot [1] of the 'words' array is null, ignores the error
-        switch (firstWord) { // Gives different outputs based on what the first word that the user entered is
+        restOfWords = words[1]; // Attempts to set restOfWords' to the second item in the array 'words'
+        switch (firstWord) { // Gives different outputs based on what the first word that the user entered
 
             // Utility Commands
 
-            // Help - 
+            // Help - Gives the user all available command, and how to use them.
             case "help":
                 System.out.println("What type of command are you looking for? (enter the number)");
                 System.out.println();
@@ -47,50 +45,50 @@ public class Game {
                     }
                     default -> System.out.println("Invalid option.");
                 }
-                playerAction();
+                playerAction(); // Sends the player back to take another action.
                 break;
 
             // Exit - The player exits the game.
             case "exit":
-                break;
+                break; // Exits the game with Code 0
 
             // General Commands
 
             // Move - The player moves in any direction that has an accessible entrance.
             case "move":
-                switch (restOfWords.toLowerCase()) {
+                switch (restOfWords) { // Runs code based off of the direction the player inputs
                     case "north":
-                        if (currentRoom.directions[0] != null) {
+                        if (currentRoom.directions[0] != null) { // Checks if the room to the north has a value
                             System.out.println("You move north.");
-                            RoomChange.enterRoom(currentRoom, currentRoom.directions[0]);
-                        }else {
+                            RoomChange.enterRoom(currentRoom, currentRoom.directions[0]); // Uses the 'enterRoom' function in the 'RoomChange' class to move the player through to another room
+                        }else { // If there is no room to the north, ignores the request and asks for a new action
                             System.out.println("Invalid option.");
                             playerAction();
                         }
                         break;
                     case "east":
-                        if (currentRoom.directions[1] != null) {
+                        if (currentRoom.directions[1] != null) { // Checks if the room to the east has a value
                             System.out.println("You move east.");
-                            RoomChange.enterRoom(currentRoom, currentRoom.directions[1]);
-                        }else {
+                            RoomChange.enterRoom(currentRoom, currentRoom.directions[1]); // Uses the 'enterRoom' function in the 'RoomChange' class to move the player through to another room
+                        }else { // If there is no room to the east, ignores the request and asks for a new action
                             System.out.println("Invalid option.");
                             playerAction();
                         }
                         break;
                     case "south":
-                        if (currentRoom.directions[2] != null) {
+                        if (currentRoom.directions[2] != null) { // Checks if the room to the south has a value
                             System.out.println("You move south.");
-                            RoomChange.enterRoom(currentRoom, currentRoom.directions[2]);
-                        }else {
+                            RoomChange.enterRoom(currentRoom, currentRoom.directions[2]); // Uses the 'enterRoom' function in the 'RoomChange' class to move the player through to another room
+                        }else { // If there is no room to the south, ignores the request and asks for a new action
                             System.out.println("Invalid option.");
                             playerAction();
                         }
                         break;
                     case "west":
-                        if (currentRoom.directions[3] != null) {
+                        if (currentRoom.directions[3] != null) { // Checks if the room to the west has a value
                             System.out.println("You move west.");
-                            RoomChange.enterRoom(currentRoom, currentRoom.directions[3]);
-                        }else {
+                            RoomChange.enterRoom(currentRoom, currentRoom.directions[3]); // Uses the 'enterRoom' function in the 'RoomChange' class to move the player through to another room
+                        }else { // If there is no room to the west, ignores the request and asks for a new action
                             System.out.println("Invalid option.");
                             playerAction();
                         }
@@ -104,9 +102,9 @@ public class Game {
 
             // Fight - The player fights an enemy that is occupying the room they are in.
             case "fight":
-                if (currentRoom.enemy != null) {
-                    fight.engage();
-                } else {
+                if (currentRoom.enemy != null) { // Checks if there is an enemy in the room
+                    fight.engage(); // Runs the 'engage' function in the 'Fight' class -> 'fight' is used as method is not static
+                } else { // If there is no enemy in the room, ignores the request and asks for a new action
                     System.out.println("There are no enemies in this room!");
                     playerAction();
                 }
