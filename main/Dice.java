77280d455d31eae7b4c40cbf8ce907439c1a257c
leepsym <frazeth@whs.school.nz>
2024-05-16 09:38:58 +1200

    Update 0.0.0.4
    Added comments to class 'Dice'
    Added comments to class 'Player'
    
    Overview:
    Added ocmments

--- main/Dice.java
+++ main/Dice.java
@@ -4,4 +4,5 @@ public class Dice {
     public static int Roll(int sides){
         return (int)(Math.random()*sides) + 1;
     }
+    // Takes an input from a user , (int), and returns a number as if a die with that many sides was rolled
 }
--- main/Player.java
+++ main/Player.java
@@ -3,6 +3,6 @@ package main;
 import item.Weapon;
 
 public class Player {
-    public static Weapon[] weapons = new Weapon[2];
-    public static int health = 30;
+    public static Weapon[] weapons = new Weapon[2]; // Creates an array that stores two objects that are instances of the class 'Weapon'
+    public static int health = 30; // Sets the global player health to 30
 }
