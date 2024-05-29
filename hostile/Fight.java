2526e67de23e0955f6300a7f82ece0c3823b8039
leepsym <frazeth@whs.school.nz>
2024-05-30 09:09:05 +1200

    Update 1.0.3.0
    Changes:
    - Lime 106 of hostile/Fight.java has been changed to include a reference to New Zealand
    
    Overview:
    - Changed text

--- hostile/Fight.java
+++ hostile/Fight.java
@@ -103,7 +103,7 @@ public class Fight {
             }
             game.playerAction(); // Sends the player back to take another action
         } else { // If the player has defeated the troll, end the game
-            System.out.println("As you glance to the south, a glimmer of daylight catches your eye, offering a tantalizing hint of escape. With a sense of relief washing over you, you think to yourself, \"Finally, a way out of here.\" Determined, you stride towards the exit, eager to leave the dark confines of the cavern behind and embrace the freedom waiting beyond.");
+            System.out.println("As you glance to the south, a glimmer of daylight catches your eye, offering a tantalizing hint of escape. With a sense of relief washing over you, you think to yourself, \"Finally, a way out of here.\" Remembering how you had been lost in the labyrinthine passages of the Waitomo Caves, you feel a surge of determination. You stride towards the exit, eager to leave the dark confines of the cavern behind and embrace the freedom waiting beyond in the lush, rolling hills."); // The Waitomo caves are a large series of underground passages in the north island of New Zealand
             System.out.println();
             System.out.println("Thank you for playing!");
         }
